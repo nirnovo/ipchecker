de4a3d314e022daf51db44e5b2c1ed008f464972
nir <nirnovo@qq.com>
2017-02-19 20:21:39 +0800

    expect bogon network error

--- ipchecker.py
+++ ipchecker.py
@@ -1,25 +1,26 @@
-#!__author__ = 'nir'
+# __author__ = 'nir'
 
 import argparse
 import requests
 import termcolor
 import pygeoip
 
-parser = argparse.ArgumentParser(prog='ip [ip]')
+parser = argparse.ArgumentParser(prog='tbip')
 parser.add_argument('ip')
 ps = vars(parser.parse_args())
+
+indent = ' ' * 8
 tb_url = 'http://ip.taobao.com/service/getIpInfo.php?ip='
 ipinfo_url = 'http://ipinfo.io/'
-indent = ' ' * 8
 
 
-def ip_check(url, ip):
+def get_ipinfo(url, ip):
     r = requests.get(url + ip)
     return r.json()
 
 
-def output_result(output, result_dic):
-    print(output.ljust(13) + ':' + indent + result_dic)
+def output_result(output, result_arr):
+    print(output.ljust(13) + ':' + indent + str(result_arr))
 
 
 def print_head(title):
@@ -27,27 +28,32 @@ def print_head(title):
     print(title)
     print('=' * 34)
 
-tb_info = ip_check(tb_url, ps['ip'])
-data = tb_info['data']
-if not tb_info['code']:
-    ua = {'User-Agent': 'curl/7.51.0'}
-    termcolor.cprint(requests.get('http://ip.cn', headers=ua).text, 'red')
-    termcolor.cprint('查询IP: ' + ps['ip'], 'yellow')
-    print_head('Taobao Result: ')
+
+ua = {'User-Agent': 'curl/7.51.0'}
+termcolor.cprint(requests.get('http://ip.cn', headers=ua).text, 'red')
+termcolor.cprint('查询IP: ' + ps['ip'], 'yellow')
+result = get_ipinfo(tb_url, ps['ip'])
+
+if not result['code']:
+    print('=' * 34)
+    data = result['data']
     for k, v in data.items():
         output_result(k, v)
+
 else:
     termcolor.cprint('查询失败!', 'yellow')
 
-
 print_head('GeoLiteCity Result: ')
+
 gi = pygeoip.GeoIP('/Users/nir/Documents/coding/GeoLiteCity.dat')
 result = gi.record_by_addr(ps['ip'])
-for k, v in result.items():
-    output_result(k, str(v))
+if result != None:
+    for k, v in result.items():
+        output_result(k, str(v))
+else:
+    print("No more info!")
 
 print_head('IPinfo.io Result: ')
-
-ipinfo = ip_check(ipinfo_url, ps['ip'])
-for k, v in ipinfo.items():
+ipinfo_data = get_ipinfo(ipinfo_url, ps['ip'])
+for k, v in ipinfo_data.items():
     output_result(k, v)
